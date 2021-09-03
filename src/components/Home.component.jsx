1c3657967996dffedb2cab80833870563034a0f2
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2021-09-03 15:08:17 +0530

    clean up

--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import axios from "axios";
 import { Dialog, Transition } from "@headlessui/react";
 import ListHome from "./ListHome.components";
